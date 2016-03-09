2c3d9834d3991f302ea45a73e6008296696c2d66
Javier Revuelta <javier.revult@gmail.com>
2016-03-09 16:38:05 -0600

    commented ruby file and set goals to guide coding process

--- test/web.rb
+++ test/web.rb
@@ -1,26 +1,32 @@
 require 'rack'
 
-#define server as server = TCPServer ( :host, :port)
-#TCPServer.accept = socket
-#socket.parse  (where parse is our method for parsing the steam, has to be unit tested)
-#****** parse will return a hash *******
-#****** hash is handed to @app *******
-#***** app returns an array of [status, headers, body] *******
-#***** socket.print/puts array *********
-#***** close the socket **********
-#***** next iteration: for multiple request handling, set up a loop *****
-#***** ATSOME POINT figure out where/when in code to close server ***********
+#Below is the ruby code for setting up a simple rack server
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#================= Steps that Still Need to be Completed =============================
+#1. [ done ] define server as server = TCPServer ( :host, :port)
+#2. [ done ] TCPServer.accept = socket
+#3. [  ]socket.parse  (where parse is our method for parsing the steam, has to be unit tested)
+#4. [  ]****** parse will return a hash *******
+#5. [  ]****** hash is handed to @app *******
+#6. [  ]***** app returns an array of [status, headers, body] *******
+#7. [  ]***** socket.print/puts array *********
+#8. [  ]***** close the socket **********
+#9. [  ]***** next iteration: for multiple request handling, set up a loop *****
+#10.[  ]***** ATSOME POINT figure out where/when in code to close server ***********
 #
 #
 #
 #***for unit test might need rack.input as a commmand of some sort ****
 #*** unit test also needs to use stringerIO to feed string *********c
+
 class Notes
   class Web
     def initialize(app, address)
       @port = address[:port]
       @host = address[:host]
       @app = app
+      new(app, address).start
     end
 
     def stop
@@ -28,7 +34,10 @@ class Notes
     end
 
     def start
-
+      server = TCPServer.new(@host, @port) #1
+      socket = server.accept #2
+      #this is where we have to parse what the socket received
+      socket.close
     end
   end
 end
