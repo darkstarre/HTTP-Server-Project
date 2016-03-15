4cbc6cab487e83cd1514a636c8a40f4bd131781e
Javier Revuelta <javier.revult@gmail.com>
2016-03-15 08:26:10 -0500

    second unit test passing - parses hash

--- test/enova_quick_test_server_to_ensure_it_works.rb
+++ test/enova_quick_test_server_to_ensure_it_works.rb
@@ -1,11 +1,11 @@
-require_relative 'web'
+require_relative '../lib/web.rb'
 
 port = 9292
 host = 'localhost'
 
 app = Proc.new do |env_hash|
 	path_info = env_hash['PATH_INFO']
-	body      = "hello, class ^_^"
+	body      = "hello, classmates ^_^"
 	[200, {'Content-Type' => 'text/plain', 'Content-Length' => body.length, 'omg' => 'bbq'}, [body]]
 end
 
