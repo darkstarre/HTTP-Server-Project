ef13b2ae2d5eb3ebcba755565dd251baaae72403
Megan Nilles <darkstarre@hotmail.com>
2016-03-16 18:31:06 -0500

    included app.rb w/in web.rb

--- lib/web.rb
+++ lib/web.rb
@@ -33,7 +33,6 @@ class Notes
       end
     end
 
-
     def self.parser(socket)
       env = {}
       method, path, http_version = socket.gets.chomp.split(" ")
@@ -51,5 +50,28 @@ class Notes
       env["BODY"] = body
       env
     end
+
+    def router
+      if path == "/form"
+        body = File.read("form.html")
+        status = "200"
+        socket.print "#{http_version} #{status} OK\r\n"
+        socket.print "Content-Type: text/html\r\n"
+        socket.print "Content-Length: #{body.length}\r\n"
+        socket.print "\r\n"
+        socket.print body
+      end
+
+      if path.start_with? "/search"
+        path, parameters = path.split("?")
+        parameters.split("&").each do |params|
+          key, value = params.split("=")
+          puts "#{key} has the value #{value}"
+        end
+      end
+    end
   end
 end
+
+server = Notes::Web.new({port: "3000", host: "localhost"})
+server.start
